7ec0651acabaebc503b7c7c74a12d2a5452e1df3
nacktordeveloppement <39088765+nacktordeveloppement@users.noreply.github.com>
2018-06-26 22:38:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('./config.json');
+const cmds =  require('./commands.json');
+const tool = require('./tool.js');
+const prompt = require('prompt');
+const colors = require('colors');
+prompt.message = "";
+prompt.delimiter = '';
 
-var prefix = "=";
+var prefix = config.prefix;
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame("=help")
+  console.log(`Bot ready in ${client.guilds.size} server!`);
+  client.user.setGame(`${prefix}help`)
 });
 
 client.on('guildMemberAdd', member => {
@@ -43,4 +50,33 @@ client.on('message', msg => {
 
 });
 
-client.login(process.env.TOKEN);
+
+bot.on('message', msg => {
+    if (msg.author.bot || msg.channel.type != 'text')
+        return; // Do not respond to messages from bots or messages that are not from guilds.
+
+    if (!msg.content.startsWith(config.prefix))
+        return; //Not a command.
+
+    let cmd = msg.content.split(/\s+/)[0].slice(config.prefix.length).toLowerCase();
+    getCmdFunction(cmd)(msg);
+});
+
+bot.on('error', (e) => console.error(e));
+bot.on('warn', (e) => console.warn(e));
+// bot.on('debug', (e) => console.info(e));
+
+bot.login(config.token);
+
+function getCmdFunction(cmd) {
+    const COMMANDS = {
+	    'ban': cmds.ban,
+        'choose': cmds.choose,
+        'help': cmds.help,
+        'debug': cmds.debug,
+        'kick': cmds.kick,
+        'prune': cmds.prune,
+        'music': music.processCommand,
+    }
+    return COMMANDS[cmd] ? COMMANDS[cmd] : () => {};
+}
